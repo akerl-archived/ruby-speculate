1ad3f9eb32ecacbb08b9838613c131f5ab431e3f
Les Aker <me@lesaker.org>
2016-12-01 16:50:05 -0800

    clean up base layout

--- lib/speculate/assumption.rb
+++ lib/speculate/assumption.rb
@@ -1,6 +1,15 @@
-##
-# Base class for assumptions
 module Speculate
+  ##
+  # Base class for assumptions
   class Assumption
+    def initialize(params = {})
+      @options = params
+    end
+
+    private
+
+    def client
+      @client ||= Aws::STS::Client.new
+    end
   end
 end
