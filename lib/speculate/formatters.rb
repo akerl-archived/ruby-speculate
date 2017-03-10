310e1db4e9e4859cb945a1f62ad9314afe1faabc
Les Aker <me@lesaker.org>
2017-03-10 11:10:49 -0500

    handle session token

--- lib/speculate/formatters.rb
+++ lib/speculate/formatters.rb
@@ -6,7 +6,8 @@ module Speculate
       @to_envvars ||= {
         AWS_ACCESS_KEY_ID: creds.access_key_id,
         AWS_SECRET_ACCESS_KEY: creds.secret_access_key,
-        AWS_SECURITY_TOKEN: creds.session_token
+        AWS_SECURITY_TOKEN: creds.session_token,
+        AWS_SESSION_TOKEN: creds.session_token
       }.map { |k, v| "export #{k}=#{v}" }.join("\n")
     end
   end
